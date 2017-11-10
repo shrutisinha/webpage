a3c7e42ca0803f43c34f90a5d5a72213ae82cd1c
shrutisinha <shrutisinha952@gmail.com>
2017-11-10 12:59:47 +0530

    Update gallery_admin_script.js

--- js/gallery_admin_script.js
+++ js/gallery_admin_script.js
@@ -1,18 +1,4 @@
 //JSON for gallery
-// var json = [
-//   {"id":"0", "src":"images/a.jpg", "name":"serenity", "info":"copied from web", "update":"10/11/2017"},
-//   {"id":"1", "src":"images/b.jpg", "name":"fox", "info":"copied from web", "update":"10/11/2017"},
-//   {"id":"2", "src":"images/c.jpg", "name":"track", "info":"copied from web", "update":"10/11/2017"},
-//   {"id":"3", "src":"images/d.jpg", "name":"cars", "info":"copied from web", "update":"10/11/2017"},
-//   {"id":"4", "src":"images/e.jpg", "name":"rose", "info":"copied from web", "update":"10/11/2017"},
-//   {"id":"5", "src":"images/f.jpg", "name":"track", "info":"copied from web", "update":"10/11/2017"},
-//   {"id":"6", "src":"images/g.jpg", "name":"tiger", "info":"copied from web", "update":"10/11/2017"},
-//   {"id":"7", "src":"images/h.jpg", "name":"saplings", "info":"copied from web", "update":"10/11/2017"},
-//   {"id":"8", "src":"images/i.jpg", "name":"fried_icecream", "info":"shruti's adventures", "update":"10/11/2017"},
-//   {"id":"9", "src":"images/j.jpg", "name":"snow", "info":"shruti's adventures", "update":"10/11/2017"},
-//   {"id":"10", "src":"images/k.jpg", "name":"sandwich", "info":"shruti's adventures", "update":"10/11/2017"},
-//   {"id":"11", "src":"images/l.jpg", "name":"sand", "info":"shruti's adventures", "update":"10/11/2017"},
-// ];
 var json = JSON.parse(pictures);
 var uniqueId = json.length;
 var gallery = document.getElementById('gallerypics1');
